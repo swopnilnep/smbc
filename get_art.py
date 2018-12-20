c73108cb5e420696983aa9974770024616783010
swopnilnep <swopnilnep@gmail.com>
2018-12-19 20:35:05 -0600

    add data entries

--- get_art.py
+++ get_art.py
@@ -4,3 +4,11 @@
 from feedparser import parse
 from random import randrange
 from xml.dom import minidom
+
+class ComicFeed:
+    '''Comic Feed Class'''
+    def __init__(self, feed_link:str):
+        rss = parse(feed_link)
+
+        # Feed data
+        self.entries = rss.entries
