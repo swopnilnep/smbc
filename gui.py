f74b9d970cc0683a4db85330dc3b3f93d37e78b0
swopnilnep <swopnilnep@gmail.com>
2019-01-17 22:54:53 -0600

    Add labels for gui

--- gui.py
+++ gui.py
@@ -1,1 +1,25 @@
 from tkinter import *
+
+# Base window of the program
+base = Tk()
+
+label_name = Label(base, text="Name")
+label_password = Label(base, text="Password")
+entry_name = Entry(base)
+entry_password = Entry(base)
+
+label_name.grid(row=0, sticky=E)
+entry_name.grid(row=0, column=1)
+label_password.grid(row=1, sticky=E)
+entry_password.grid(row=1, column=1)
+
+def print_name():
+    print("Hello World!")
+
+c = Checkbutton(base, text="Keep me logged in")
+c.grid(columnspan=2)
+
+button1 = Button(base, command=print_name)
+
+# Loop until cross is pressed
+base.mainloop()
